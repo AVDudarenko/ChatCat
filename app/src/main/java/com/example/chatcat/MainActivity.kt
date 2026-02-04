83c246f08b3553241de47b6d9ee6aa65646d642d
aleksandr <aleksandr.dudarenko@gmail.com>
2026-02-04 14:12:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/chatcat/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.chatcat
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+}
